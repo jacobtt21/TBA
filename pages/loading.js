c380cfbb2576e08dbeaba9681378c5715162b2fd
jacobtt21 <jacobt1206@gmail.com>
2022-11-06 13:03:01 -0600

    new loading page

--- pages/loading.js
+++ pages/loading.js
@@ -4,7 +4,11 @@ function loading() {
     <div className="w-4/5 md:w-1/2 mx-auto">
       <div className="container mt-16 flex justify-center">
         <h3 className="font-bold flex flex-wrap justify-center text-2xl">
-          Loading ...
+          <img
+          src="https://github.com/Oustro/OustroImages/blob/main/Untitled%20design.gif?raw=true"
+          className="pt-20 w-72"
+          alt="..."
+          />
         </h3>
       </div>
     </div>
