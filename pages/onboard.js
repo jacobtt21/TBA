f8a98e4bbfd306d52cad8a279d065978bd4705e2
jacobtt21 <jacobt1206@gmail.com>
2023-04-20 13:48:01 -0500

    done with onboard

--- pages/onboard.js
+++ pages/onboard.js
@@ -7,18 +7,25 @@ function Onboard() {
   const userExtra = useContext(UserContextExtra);
 
   return user && userExtra ? (
-    <div className="w-4/5 md:w-1/2 mx-auto">
-      <div className="mx-auto justify-center text-center items-center">
-        <h3 className="font-bold mt-16 text-4xl">
-          We're Glad <span className="bg-yellow-400 rounded-lg p-1">You're</span> Here
+    <div className='bg-scroll bg-contain fixed overflow-auto h-full w-full no-scrollbar bg-hero'>
+      <div className="container w-11/12 mx-auto mt-16 justify-center">
+        <h3 className="font-bold text-center mt-16 text-4xl">
+          We're Glad <span className="bg-yellow-400 rounded-lg p-2">You're</span> Here
         </h3>
-        <h3 className="font-bold mt-8 text-2xl">something nice about TBA, Oustro, and then the rules and links to website like that</h3>
+        <h3 className="mt-8 text-center text-2xl">
+          Welcome to The Birthday App, the app that helps you celebrate birthdays with your friends! 
+          Use our in-app features to send gifts and wish the very special people in your life a very happy birthday!
+        </h3>
+
+        <h3 className="mt-8 font-bold text-center text-2xl">
+          Currently we are in beta and are looking for feedback, so please let us know by reaching out with suggestions!
+        </h3>
       </div>
       <div className='mx-auto flex justify-center'>
         <div className="flex justify-end items-center pt-8">
           <Link href="/">
             <button className="btn-yellow text-2xl">
-              I accept
+              Let's go!
             </button>
           </Link>
         </div>
